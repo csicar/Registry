2f60bacc7427a4093be581de0e98cf483ec56ea1
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2014-04-11 01:25:46 +0200

    asda

--- libs/mathjs.js
+++ libs/mathjs.js
@@ -1,1 +1,3 @@
-yepnope('http://cdnjs.cloudflare.com/ajax/libs/mathjs/0.19.0/math.min.js')
+yepnope(['http://mathjs.org/js/lib/es5-sham.min.js',
+	'http://mathjs.org/js/lib/es5-shim.min.js',
+	'http://cdnjs.cloudflare.com/ajax/libs/mathjs/0.19.0/math.min.js'])
